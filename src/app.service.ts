66b2b601e2d0c67cee5716fcb8ab78b70c932531
Sameh Dheir <dheir2000@gmail.com>
2026-04-24 18:39:31 +0300

    test 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  onModuleInit() {
+    console.log('🚀 App started, waiting 5 seconds to trigger error...');
+    
+    setTimeout(() => {
+      // هذا الخطأ سيحدث خارج نطاق NestJS (Unhandled)
+      // وسيجبر الـ monitor على البدء فوراً
+      const user: any = null;
+      console.log(user.name); 
+    }, 5000);
   }
 }
