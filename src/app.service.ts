1559932b2ad9b515999e5a38821cc7b3fcd3df4b
Sameh Dheir <98809352+SamehDheir@users.noreply.github.com>
2026-05-02 14:24:00 +0300

    AutoPR: Fix for 💥 Catastrophic error: Database connection failed!

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,33 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 
+export class TimeoutError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'TimeoutError';
+  }
+}
+
 @Injectable()
 export class AppService implements OnModuleInit {
   
   onModuleInit() {
     console.log('⏳ An asynchronous process is running that will fail after 3 seconds...');
 
-    this.simulatedAsyncOperation();
+    this.simulatedAsyncOperation().catch((error) => {
+      console.error('Error occurred:', error);
+    });
   }
 
   async simulatedAsyncOperation() {
-    await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject(new Error('💥 Catastrophic error: Database connection failed!'));
-      }, 3000);
-    });
+    try {
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          reject(new TimeoutError('💥 Catastrophic error: Database connection failed!'));
+        }, 3000);
+      });
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
   }
 
   getHello(): string {
