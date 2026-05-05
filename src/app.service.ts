26756bffa25f1cb51e686f5f5cc611883083531f
Sameh Dheir <98809352+SamehDheir@users.noreply.github.com>
2026-05-05 12:43:33 +0300

    AutoPR: Fix for Cannot read properties of null (reading 'name')

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,30 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 
+export class TimeoutError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
 @Injectable()
 export class AppService implements OnModuleInit {
   onModuleInit() {
-    console.log('🕒 An asynchronous process is running that will succeed...');
+    console.log('\u{1F352} An asynchronous process is running that will succeed...');
 
-    this.simulatedAsyncOperation().catch((error) => console.error(error));
+    this.simulatedAsyncOperation().catch((error) => {
+      if (error instanceof TimeoutError) {
+        console.error('💔 Operation timed out:', error.message);
+      } else {
+        console.error('🤕 Oops, something went wrong:', error.message);
+      }
+    });
   }
 
   async simulatedAsyncOperation() {
     try {
       return await new Promise((resolve, reject) => {
         setTimeout(() => {
-          reject(new Error('❌ Failed to connect to the database!'));
+          reject(new TimeoutError('❌ Operation timed out!'));
         }, 3000);
       });
     } catch (error: any) {
