576682f7d87656491e5efb7424886bc525c5de75
Sameh Dheir <dheir2000@gmail.com>
2026-05-05 12:34:48 +0300

    broblem 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,22 +2,21 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class AppService implements OnModuleInit {
-  
   onModuleInit() {
     console.log('🕒 An asynchronous process is running that will succeed...');
 
-    this.simulatedAsyncOperation().catch(error => console.error(error));
+    this.simulatedAsyncOperation().catch((error) => console.error(error));
   }
 
   async simulatedAsyncOperation() {
     try {
       return await new Promise((resolve, reject) => {
         setTimeout(() => {
-          resolve('Database connection was successful!');
+          reject(new Error('❌ Failed to connect to the database!'));
         }, 3000);
       });
-    } catch (error) {
-      console.error(error);
+    } catch (error: any) {
+      console.error('Captured in catch block:', error.message);
     }
   }
 
